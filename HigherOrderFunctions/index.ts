e7f9c27a51fcb7e509afd5c295969219bb3825c2
Aman Bhardwaj <inblack>
2021-02-22 20:37:52 +0530

    rest I know

---
+++ HigherOrderFunctions/index.ts
@@ -0,0 +1,3 @@
+// Higher Order Functions are functions that can be passed as input to other functions
+// or functions that can be returned from other functions as output.It is enough for a function to fulfill one of these conditions to be considered a higher - order function.
+
---
+++ Predicates/index.ts
@@ -0,0 +1,3 @@
+// The term « predicate » comes from mathematics. In programming it refers to a function which
+// returns either true or false.We can imagine that predicates are like questions.Usually we name;
+// such functions as questions.Higher - order functions may take predicates as input; one of such functions is the filter, defined as a method on the Array object.;
